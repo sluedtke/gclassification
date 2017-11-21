874c445e882ce8f018bb0408314e3b6642dc3281
Stefan Lüdtke <sluedtke@gfz-potsdam.de>
2017-11-21 10:22:00 +0100

    Implemented a "reduce_tree" function
    That helps to create parameter regions.

--- R/gclassificatin_func.R
+++ R/gclassificatin_func.R
@@ -1,42 +1,78 @@
-#  -------------------------------------------------------------
-
-#' How to document functions.
-#'
-#' This is an example showing how to document functions to be included in the package.
-#'
+#' A function that extracts the nodes of a data.tree object where the given
+#' attribute is not NA, not called by the user
 #' @export
-#' @name parse_outlet
+#' @name extract_node_v_helper
 #' @author Stefan Lüdtke
-#' @param text Message to print.
-#' @seealso \url{http://r-pkgs.had.co.nz/man.html}
+#' @param  data_tree A data.tree object
+#' @param  attribute Character string that specifies the attribute of interest 
 #' @examples
-#' parse_outlet('Hello world!')
+#' test = extract_node_v(data_tree, attribute = "station_id")
 
-parse_outlet = function(node, func_station_id = NA) {
-  result = node$station_id
-  if(is.null(result)){
-    node$station_id = func_station_id
-    return(node)
-  } else{
-    result =  sapply(node$children, parse_outlet, func_station_id = result)
-    return(node)
+extract_node_v_helper = function(data_tree, attribute) {
+  all_fields = data_tree$fieldsAll
+  # check if the desired field exists in the tree
+  if (any(attribute %in% all_fields)){
+        temp_df = data.tree::ToDataFrameNetwork(data_tree, attribute) %>%
+          filter(complete.cases(.))
+  } else{ 
+    # break if not
+    stop(paste0("Attribute ", attribute, " is not in the list of fields\n", 
+                "Available fields are: ", all_fields))
   }
+  # we extract the "to" node and return a vector
+  return(as.vector(temp_df[, 'to']))
 }
-
 #  -------------------------------------------------------------
 
-#' How to document functions.
-#'
-#' This is an example showing how to document functions to be included in the package.
-#'
+#' A helper function that is not called by the user
 #' @export
-#' @name examplefunction
-#' @author Francisco Rodriguez-Sanchez
-#' @param text Message to print.
-#' @seealso \url{http://r-pkgs.had.co.nz/man.html}
-#' @examples
-#' examplefunction('Hello world!')
+#' @name reduce_tree_helper
+#' @author Stefan Lüdtke
+#' @param  x A node name to start with
+#' @param  data_tree A tree to operate on 
+#' @return a data frame with 1 row and the node name from the next ancestor 
 
-examplefunction <- function(text){
-  return(text)
+reduce_tree_helper =  function(x, data_tree, attribute) {
+  # find the subtree of the node_id of interest
+  temp_tree = FindNode(data_tree, x)
+  # extract the levels of the tree but only for ancestors
+  temp_df = data.frame(level = temp_tree$Get('level', traversal = "ancestor"), 
+                       name = temp_tree$Get('name', traversal = "ancestor"),
+                       att = temp_tree$Get(attribute, traversal = "ancestor")) %>%
+    # attach the source node
+    mutate(to = x) %>%  dplyr::rename(from = name) %>%
+    # filter/discard each station where the attribute is not missing
+    filter(!is.na(att)) %>%
+    # filter/discard each station with the same from-to combination
+    filter(from != to) %>%
+    # select the closest node that has the attribute
+    filter(level == max(level)) %>% 
+    # keep only the columns of interest
+    select(-level, -att)
+  if (nrow(temp_df) == 0) {
+    return_df = NULL
+  }else {
+    return_df = temp_df
+  }
+  return(return_df)
 }
+#  -------------------------------------------------------------
+
+#' A function that creates new (reduced) tree based on an attribute that must
+#' not be NA
+#' @export
+#' @name reduce_tree
+#' @author Stefan Lüdtke
+#' @param  a data.tree object that provides the source
+#' @param  a list/vector of nodes that build up the tree
+#' @return a data.tree object 
+#' @examples
+#' test_tree_reduced = reduce_tree(base_tree_b, attribute = 'station_id')
+
+reduce_tree = function(data_tree, attribute) {
+  node_ids = extract_node_v(data_tree, attribute)
+  tree_reduced = ldply(node_ids, reduce_tree_helper, data_tree, attribute) %>% 
+    FromDataFrameNetwork()
+  return(tree_reduced)
+}
+#  -------------------------------------------------------------
--- tests/testthat/test_classification.R
+++ tests/testthat/test_classification.R
@@ -1,76 +1,92 @@
 #  ------------------------------
 
 library(testthat)
+library(plyr)
 library(tidyverse)
 library(data.tree)
 
 #  ------------------------------
-mapping_df =  data.frame(subbasinID = c(1, 2, 6, 7, 12),
-                      station_id = c("a", "b", "c", "d", "e"), 
+mapping_df_a =  data.frame(subbasinID = c(1, 2, 6, 7, 12),
+                      station_id = c("a", "b", "c", "d", "e"),
                       stringsAsFactors = F)
 #
 # create a test tree that we think we understand quite well
-test_tree = read_tsv("../../data/example_tree.tsv") %>%
-  left_join(mapping_df) %>%
-  rename(Parent = subbasinID, Child = nextID) %>%
+base_tree_a = read_tsv("../../data/example_tree.tsv") %>%
+  dplyr::left_join(mapping_df_a) %>%
+  dplyr::rename(Parent = subbasinID, Child = nextID) %>%
   as.data.frame() %>%
-  FromDataFrameNetwork() 
+  data.tree::FromDataFrameNetwork()
 
+#  ------------------------------
+mapping_df_b =  data.frame(subbasinID = c(1, 3, 7, 8, 12, 14),
+                      station_id = c("a", "b", "c", "d", "e", "f"),
+                      stringsAsFactors = F)
+#
+# create a test tree that we think we understand quite well
+base_tree_b = read_tsv("../../data/example_tree.tsv") %>%
+  dplyr::left_join(mapping_df_b) %>%
+  dplyr::rename(Parent = subbasinID, Child = nextID) %>%
+  as.data.frame() %>%
+  data.tree::FromDataFrameNetwork()
+
+test_tree = data.tree::Clone(base_tree_a)
+
+#  ------------------------------
+#  ------------------------------
 context("Create the tree object from the textfile:
         Tree looks like that:
         #  ------------------------------
-        # 0
-        # ¦--1
-        #    ¦--3
-        #    ¦   ¦--7
-        #    ¦   ¦   ¦--8
-        #    ¦   ¦   ¦   ¦--11
-        #    ¦   ¦   ¦   ¦--12
-        #    ¦   ¦   ¦   ¦   °--14
-        #    ¦   ¦   ¦   °--13
-        #    ¦   ¦   ¦--9
-        #    ¦   ¦   °--10
-        #    ¦   °--6
-        #    °--2
-        #        ¦--4
-        #        °--5
+        # -1
+        #  ¦--1
+        #     ¦--3
+        #     ¦   ¦--7
+        #     ¦   ¦   ¦--8
+        #     ¦   ¦   ¦   ¦--11
+        #     ¦   ¦   ¦   ¦--12
+        #     ¦   ¦   ¦   ¦   °--14
+        #     ¦   ¦   ¦   °--13
+        #     ¦   ¦   ¦--9
+        #     ¦   ¦   °--10
+        #     ¦   °--6
+        #     °--2
+        #         ¦--4
+        #         °--5
         #  ------------------------------\n")
-
-
 #  -------------------------------------------------------------
+
 context("Testing my understanding of the functions with regard to leaves and
         levels provided by the package")
 
 # Test whether all node names are unique.
 test_that("Test unique names", {
-              expect_true(AreNamesUnique(test_tree))
+              expect_true(AreNamesUnique(base_tree_a))
 })
 
 # Number of leaves
 test_that("Test the number of leaves", {
               # final leaves
-              expect_equal(length(test_tree$leaves), 8)
+              expect_equal(length(base_tree_a$leaves), 8)
               # within the tree
               # Here we find 11, 14, 13, so 3 leaves
-              expect_equal(length(FindNode(test_tree, "8")$leaves), 3)
+              expect_equal(length(FindNode(base_tree_a, "8")$leaves), 3)
               # Here we find 14 only so a singe leave
-              expect_equal(length(FindNode(test_tree, "12")$leaves), 1)
+              expect_equal(length(FindNode(base_tree_a, "12")$leaves), 1)
 })
 
 # The number of levels or the hierarchy of the tree
 test_that("Test the number of levels", {
               # returns a named vector (node names) with the associated level
               # We have 6 levels (at node 14) in total
-              tree_levels = test_tree$Get("level")
+              tree_levels = base_tree_a$Get("level")
               expect_equal(max(tree_levels), 7)
               # we have level 1 at node 1
               expect_equal(as.numeric(tree_levels["1"]), 2)
               # that should be the same if we "find" the node
-              expect_equal(as.numeric(FindNode(test_tree, "1")$level), 2)
+              expect_equal(as.numeric(FindNode(base_tree_a, "1")$level), 2)
               # we have level 4 at node 10
               expect_equal(as.numeric(tree_levels["10"]), 5)
               # that should be the same if we "find" the node
-              expect_equal(as.numeric(FindNode(test_tree, "10")$level), 5)
+              expect_equal(as.numeric(FindNode(base_tree_a, "10")$level), 5)
 })
 
 #  -------------------------------------------------------------
@@ -78,8 +94,9 @@ context("Testing my understanding with regard to clone and subtree functions
         provided by the package")
 
 # cloning a subtree
-sub_a = Clone(FindNode(test_tree, "12"))
-sub_b = Clone(FindNode(test_tree, "8"))
+sub_a = Clone(FindNode(base_tree_a, "12"))
+sub_b = Clone(FindNode(base_tree_a, "8"))
+sub_c = Clone(FindNode(base_tree_a, "7"))
 
 test_that("Test the extracting and cloning subtrees", {
               expect_true(isRoot(sub_a$"12"))
@@ -93,10 +110,41 @@ test_that("Check the new levels", {
 })
 
 #  -------------------------------------------------------------
-context("Check the function to parse values through the tree")
+context("Check the function to extract only nodes with a required attribute")
 
-test_that('parsing node attributes', {
-            temp_df = data.tree::ToDataFrameTable(test_tree, "name", "station_id") %>%
-              arrange(name)
+test_that("Extract only nodes with a required attribute from cloned trees", {
+            test_a = extract_node_v(sub_a, attribute = "station_id")
+            expect_equal(character(0),  test_a)
+            test_b = extract_node_v(sub_b, attribute = "station_id")
+            expect_equal("12",  test_b)
+            test_c = extract_node_v(sub_c, attribute = "station_id")
+            expect_equal("12", test_c)
 })
 
+test_that("Extract only nodes with a required attribute from the base tree", {
+            test_vect = extract_node_v(base_tree_a, attribute = "station_id")
+            expect_equal(sort(c("1", "2", "6", "7", "12")), sort(test_vect))
+            # that one should fail
+            expect_error(extract_node_v(base_tree_a, attribute = "false_column"))
+})
+
+
+#  -------------------------------------------------------------
+context("Check the function to reduce the tree to nodes with a required
+        attribute only")
+
+test_that("Test the reduced tree on example base_tree_a", {
+            test_tree_reduced = reduce_tree(base_tree_a,
+                                            attribute = 'station_id')
+            test_reduced = test_tree_reduced$Get('height')
+            names(test_reduced) = NULL
+            expect_equal(sort(c(1, 1, 1, 2, 3)), sort(test_reduced))
+})
+
+test_that("Test the reduced tree on example base_tree_b", {
+        test_tree_reduced = reduce_tree(base_tree_b,
+                                        attribute = 'station_id')
+            test_reduced = test_tree_reduced$Get('height')
+            names(test_reduced) = NULL
+            expect_equal(sort(c(1, 2, 3, 4, 5, 6)), sort(test_reduced))
+})
